3929e83cdeab68d38dc07314d7cf0cf899c3b20d
Daulton Baird <daultonbaird1@gmail.com>
2021-05-23 00:04:03 -0700

    Update OWLeague.py
    Added main function
    Fixed the "48 hour time" conversion
    Made print statements more readable
    Added line comments for readability

--- OWLeague.py
+++ OWLeague.py
@@ -1,3 +1,4 @@
+import sys
 import mouse
 import time
 import webbrowser
@@ -6,23 +7,20 @@ from pynput.keyboard import Key, Controller
 import clipboard
 
 
-#print(mouse.get_position())
+def set_browsers():    # set up browsers to be used with webbrowser library
+    ff_path = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+    webbrowser.register('Firefox', None, webbrowser.GenericBrowser(ff_path))
+
+    edge_path = 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
+    webbrowser.register('Edge', None, webbrowser.BackgroundBrowser(edge_path))
+
+    brave_path = "C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+    webbrowser.register('Brave', None, webbrowser.GenericBrowser(brave_path))
+
+    # print('tryorder:', webbrowser._tryorder)    # use this to print list of usable browsers
 
 
-ffpath = "C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-webbrowser.register('firefox', None, webbrowser.GenericBrowser(ffpath))
-
-edgepath = 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe'
-webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edgepath))
-
-bravepath = "C:\\Program Files (x86)\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
-webbrowser.register('brave', None, webbrowser.GenericBrowser(bravepath))
-
-# use this to print list of usable browsers
-#print('tryorder:', webbrowser._tryorder)
-
-
-def openIn(browser):  # this function opens the stream in the browser parameter. The browser MUST be defined above!!!
+def open_in(browser):  # this function opens the stream in the browser parameter. The browser MUST be defined above!!!
     # move to the stream URL
     mouse.move(475, 52, absolute=True, duration=0.2)
     mouse.click('left')
@@ -39,92 +37,100 @@ def openIn(browser):  # this function opens the stream in the browser parameter.
     keyboard.release(Key.ctrl)
     time.sleep(1)
 
-    newURL = clipboard.paste()  # text will have the content of clipboard
+    new_URL = clipboard.paste()  # new_URL will be copied into clipboard
     # print('newURL:', newURL)
-    newBrowser = webbrowser.get(browser)
-    newBrowser.open(newURL)
+    new_browser = webbrowser.get(browser)
+    new_browser.open(new_URL)
     print(f'\n~~~Overwatch League now playing on {browser}~~~')
 
 
-''''''
+def find_time_diff(match_time, curr_time):    # returns time difference between match and curr time in full time and in seconds
+    hour_diff = int(match_time[:2]) - int(curr_time[:2])
+    if hour_diff < 0:
+        hour_diff = 0
 
-match_time = ''
-# input validation
-while ':' not in match_time:
-    match_time = input('Enter time of match in 24hr time format "xx:xx": ')
+    min_diff = int(match_time[3:5]) - int(curr_time[3:5])
+    if min_diff < 0:
+        min_diff = abs(60 + min_diff)
+        hour_diff -= 1
 
-# validation for time in AM (ex. 2:00AM)
-if len(match_time) == 4:
-    match_time = '0'+match_time
+    sec_diff = int(match_time[6:8]) - int(curr_time[6:8])
+    if sec_diff < 0:
+        sec_diff = abs(60 + sec_diff)
+        min_diff -= 1
 
-match_time = match_time + ':00'  # add seconds
-curr_time = str(datetime.datetime.now())[11:19]
-print(f'Time is currently: {curr_time}')
+    if min_diff <= 0:
+        min_diff = 0
+    time_diff = datetime.time(hour_diff, min_diff, sec_diff)
+    print(f'Match is in {time_diff}')
 
-a = datetime.time(int(curr_time[:2]), int(curr_time[3:5]), int(curr_time[6:8]))  # see datetime docs
+    time_diff_sec = (hour_diff * 3600) + (min_diff * 60) + sec_diff
+    # print(f'Match is in {time_diff_sec} seconds')    # for debugging
+    return time_diff, time_diff_sec
 
-# these two ifs convert to 48 hour time. This is useful if the game is at 1:00AM and it is currently 11:00PM.
-# if they are both less than 12 it means the match is on the same day
-if int(curr_time[:2]) < 12 & int(match_time[:2]) >= 12:
-    new_currTime = str(int(curr_time[:2])+12)
-    curr_time = new_currTime + ':' + curr_time[3:]
-    print(f'curr_time to 48hr: {curr_time}')
 
-# if they are both less than 12 it means the match is on the same day
-if int(match_time[:2]) < 12 & int(curr_time[:2]) >= 12:
-    new_matchTime = str(int(match_time[:2]) + 24)
-    match_time = new_matchTime + ':' + match_time[3:]
-    print(f'match_time to 48hr: {match_time}')
+def main():
+    match_time = ''
+    # print(mouse.get_position())   # prints current mouse position, used to tweak the clicking commands.
 
-hourDiff = int(match_time[:2]) - int(curr_time[:2])
-if hourDiff < 0:
-    hourDiff = 0
+    while ':' not in match_time:    # input validation
+        match_time = input('Enter time of match in 24hr time format "xx:xx": ')
 
-minDiff = int(match_time[3:5]) - int(curr_time[3:5])
-if minDiff < 0:
-    minDiff = abs(60 + minDiff)
-    hourDiff -= 1
+    if len(match_time) == 4:    # validation for time in AM not including leading 0 (ex. 2:00AM)
+        match_time = '0'+match_time
 
-secDiff = int(match_time[6:8]) - int(curr_time[6:8])
-if secDiff < 0:
-    secDiff = abs(60 + secDiff)
-    minDiff -= 1
+    match_time = match_time + ':00'    # add seconds to match time
+    curr_time = str(datetime.datetime.now())[11:19]    # get current time
+    print(f'Time is currently: {curr_time}')
 
-if minDiff <= 0:
-    minDiff = 0
-timeDiff = datetime.time(hourDiff, minDiff, secDiff)
-print(f'Match is in {timeDiff} ')
+    # a = datetime.time(int(curr_time[:2]), int(curr_time[3:5]), int(curr_time[6:8]))  # see datetime docs
 
-timeDiff_sec = (hourDiff * 3600) + (minDiff * 60) + secDiff
-print(f'Match is in {timeDiff_sec} seconds')
+    new_match_time = match_time  # set this as a default value (match is same day)
 
-while True:
-    curr_time = str(datetime.datetime.now())[11:19]
-    print(f'comparing {match_time[:5]} and {curr_time[:5]}')
-    if match_time[:5] in curr_time[:5]:
+    ''' If the match is in the morning of the next day, we need to use "48 hr time"
+    # We add 24 hours to the match_time given so that the subtraction of new_match_time - curr_time is accurate instead of negative.
+    # Example using current time 11:00PM and match time 1:00AM:'''
+    #   match_time[:2] = 01          curr_time[:2] = 23
+    if int(match_time[:2]) < 12 and int(curr_time[:2]) >= 12:
+        new_match_time = str(int(match_time[:2]) + 24)   # 01 + 24 = 25
+        new_match_time = new_match_time + ':' + match_time[3:]
+        print(f'new match time to 48hr: {new_match_time}')  # new match time to 48hr: 25:00:00
 
-        print(f"\nTime is currently: {curr_time[:5]}, which means It's Overwatch time baby!~~~~\n ")
-        time.sleep(1)
-        url = 'https://www.youtube.com/c/overwatchleague'
+    first = True    # first run in loop variable
+    time_diff, time_diff_sec = find_time_diff(new_match_time, curr_time)    # if it is the first run in while loop, use new match time in 48hr time
 
-        ff = webbrowser.get('firefox')
-        ff.open(url)
+    while True:
+        curr_time = str(datetime.datetime.now())[11:19]    # get current time to compare to
+        # print(f'comparing {match_time[:5]} and {curr_time[:5]}')
 
-        time.sleep(4)
-        # move mouse to upcoming live stream button coords
-        mouse.move(524, 950, absolute=True, duration=0.2)
-        # print(f'Upcoming live stream coords: {mouse.get_position()}')
-        # left click on stream
-        mouse.click('left')
-        time.sleep(1)
-        print('\n~~~Overwatch League now playing on Firefox~~~')
+        if not first:    # if it is the second time, the comparison was wrong so use the current time
+            time_diff, time_diff_sec = find_time_diff(match_time, match_time)    # get the new time diff (should not be needed, but added for safety)
 
-        # optional: add multiple browsers with the same url (multiple accounts).
-        openIn('edge')
-        openIn('brave')
-        time.sleep(2)
-        break
+        if match_time[:5] in curr_time[:5]:    # if the match time is IN current time it means within the minute. Change to [:4] for within 10 minutes.
+            print(f"\nTime is currently: {curr_time[:5]}, which means It's Overwatch time baby!~~~~\n ")
+            url = 'https://www.youtube.com/c/overwatchleague'    # this links to the Overwatch League's YouTube channel
+            ff = webbrowser.get('Firefox')    # get the firefox browser
+            ff.open(url)    # open the url in firefox
+            time.sleep(4)    # sleep for 4 seconds to allow load time
+            mouse.move(524, 950, absolute=True, duration=0.2)    # move mouse to upcoming live stream button coords
+            # print(f'Upcoming live stream coords: {mouse.get_position()}')
+            mouse.click('left')    # open stream
+            time.sleep(1)
+            print('\n~~~Overwatch League now playing on Firefox~~~')
 
-    else:
-        print(f'sleeping for {timeDiff_sec} seconds')
-        time.sleep(timeDiff_sec)
+            # optional: add multiple browsers with the same url (multiple accounts).
+            open_in('Edge')
+            open_in('Brave')
+            time.sleep(1)
+            break
+
+        else:
+            print(f'sleeping for {str(time_diff)[:2]} hour(s), {str(time_diff)[3:5]} mins, and {str(time_diff)[6:]} seconds')
+            time.sleep(time_diff_sec)    # sleep until game time
+            first = False    # not the first iteration in the loop
+
+    sys.exit(0)    # exit the code
+
+
+if __name__ == "__main__":
+    main()
